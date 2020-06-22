81fbd25bda349540573a8f3f12c8416b42f3d0cc
Gleb Pautov <pautov.split@mail.ru>
2020-06-22 12:39:21 +0500

    done

---
+++ zad7.2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace zad7._2
+{
+    class Program
+    {
+        class Fuck
+        {
+            public int a;
+            public int b;
+
+            public void PrintNums()
+            {
+                Console.WriteLine("a = " + a + "\nb = " + b + "\n");
+            }
+
+            public void ChangeNums(int A, int B)
+            {
+                a = A;
+                b = B;
+            }
+
+            public void Sum()
+            {
+                Console.WriteLine("a + b = " + (a + b) + "\n");
+            }
+
+            public void MaxNum()
+            {
+                if (a > b) Console.WriteLine(a + " > " + b + "\n");
+                if (b > a) Console.WriteLine(b + " > " + a + "\n");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Fuck f = new Fuck();
+            f.a = 12;
+            f.b = 7;
+            f.PrintNums();
+            f.Sum();
+            f.MaxNum();
+            f.ChangeNums(5, 2);
+            f.PrintNums();
+            f.Sum();
+            f.MaxNum();
+        }
+    }
+}
